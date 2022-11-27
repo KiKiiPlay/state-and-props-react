d74d959fced92c36e54f29254989ce828c33c4b2
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-27 18:26:19 +0100

    Creating Search bar and it's functionality for book shop

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,41 @@
 import SingleBook from "./SingleBook";
+import { Component } from "react";
+import Form from "react-bootstrap/Form";
 
-const BookList = ({ arr }) => {
-  return (
-    <>
-      <div className="d-flex flex-wrap justify-content-center">
-        {arr.map((book) => {
-          return <SingleBook book={book} />;
-        })}
-      </div>
-    </>
-  );
-};
+class BookList extends Component {
+  state = {
+    searchQuery: "",
+  };
+
+  render() {
+    return (
+      <>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Search</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Search here"
+              value={this.state.searchQuery}
+              onChange={(e) => this.setState({ searchQuery: e.target.value })}
+            />
+            <Form.Text className="text-muted">
+              Here you can find anything you want
+            </Form.Text>
+          </Form.Group>
+        </Form>
+        <div className="d-flex flex-wrap justify-content-center">
+          {this.props.arr
+            .filter((n) =>
+              n.title.toLowerCase().includes(this.state.searchQuery)
+            )
+            .map((book) => {
+              return <SingleBook book={book} />;
+            })}
+        </div>
+      </>
+    );
+  }
+}
 
 export default BookList;
