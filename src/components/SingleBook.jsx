5629c20848d151414bb27bc5828709ca4f3cb109
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-27 18:29:06 +0100

    Convering component from function to class

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,14 +1,19 @@
 import Card from "react-bootstrap/Card";
+import { Component } from "react";
 
-const SingleBook = ({ book }) => (
-  <>
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={book.img} />
-      <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-      </Card.Body>
-    </Card>
-  </>
-);
+class SingleBook extends Component {
+  render() {
+    return (
+      <>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={this.props.book.img} />
+          <Card.Body>
+            <Card.Title>{this.props.book.title}</Card.Title>
+          </Card.Body>
+        </Card>
+      </>
+    );
+  }
+}
 
 export default SingleBook;
