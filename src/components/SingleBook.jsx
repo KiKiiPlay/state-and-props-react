4ba86360d55749d57775c9ca89e1e72af8b44891
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-27 18:51:22 +0100

    Completing functionality of application

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,10 +2,21 @@ import Card from "react-bootstrap/Card";
 import { Component } from "react";
 
 class SingleBook extends Component {
+  state = {
+    selected: false,
+    id: "",
+  };
+
   render() {
     return (
       <>
-        <Card style={{ width: "18rem" }}>
+        <Card
+          onClick={() => this.setState({ selected: !this.state.selected })}
+          style={{
+            width: "18rem",
+            border: this.state.selected ? "3px red solid" : "none",
+          }}
+        >
           <Card.Img variant="top" src={this.props.book.img} />
           <Card.Body>
             <Card.Title>{this.props.book.title}</Card.Title>
